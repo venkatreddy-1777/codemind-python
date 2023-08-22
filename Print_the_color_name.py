f45cb32422385c5bb45579cfd87aa7d192c5a77c
venkatreddy-1777 <119719993+venkatreddy-1777@users.noreply.github.com>
2023-08-22 19:51:34 +0530

    ch=input() if(ch=='V'):     print("Violet") elif(ch=='I'):     print("Indigo") elif(ch=='B'):     print("Blue") elif(ch=='G'):     print("Green") elif(ch=='Y'):     print("Yellow") elif(ch=='O'):     print("Orange") elif(ch=='R'):     print("Red") else:     print("-1")

---
+++ Print_the_color_name.py
@@ -0,0 +1,17 @@
+ch=input()
+if(ch=='V'):
+    print("Violet")
+elif(ch=='I'):
+    print("Indigo")
+elif(ch=='B'):
+    print("Blue")
+elif(ch=='G'):
+    print("Green")
+elif(ch=='Y'):
+    print("Yellow")
+elif(ch=='O'):
+    print("Orange")
+elif(ch=='R'):
+    print("Red")
+else:
+    print("-1")
