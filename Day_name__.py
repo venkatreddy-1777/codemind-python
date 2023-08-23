1fd6f43e99e408a1783118e4dfd687a69c975481
venkatreddy-1777 <119719993+venkatreddy-1777@users.noreply.github.com>
2023-08-23 19:52:29 +0530

    d=int(input()) if(d==1):     print("Monday") elif(d==2):     print("Tuesday") elif(d==3):     print("Wednesday") elif(d==4):     print("Thursday") elif(d==5):     print("Friday") elif(d==6):     print("Saturday") else:     print("Invalid input")

---
+++ Day_name__.py
@@ -0,0 +1,15 @@
+d=int(input())
+if(d==1):
+    print("Monday")
+elif(d==2):
+    print("Tuesday")
+elif(d==3):
+    print("Wednesday")
+elif(d==4):
+    print("Thursday")
+elif(d==5):
+    print("Friday")
+elif(d==6):
+    print("Saturday")
+else:
+    print("Invalid input")
